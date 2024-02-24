f5fcb5a9ce1d901b5ae03d6daa9a025e3148a462
Edgar Mejia <edgarinhm@hotmail.com>
2024-02-24 13:01:30 -0500

    UD-NEXT-JS-01 add images UD-NEXT-JS-01 add public relative mask route UD-NEXT-JS-01 refactor header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "public/images/home.jpg";
+
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
       Home Page
+      <Image
+        src={homeImg}
+        alt="car factory"
+        fill
+        style={{ objectFit: "cover" }}
+      />
     </div>
   );
 }
